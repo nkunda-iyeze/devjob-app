cb3c78aa5a1e886cadffd1f93ea601a03ed9fb1a
nkunda-iyeze <shalom.iyeze@gmail.com>
2023-05-17 03:23:49 +0200

    Modified Navbar Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
 
+import logo from '../assets/desktop/logo.svg'
+import bgPatternHeader from '../assets/desktop/bg-pattern-header.svg'
+import {BsFillSunFill,BsToggleOff,BsFillMoonFill } from "react-icons/bs";
 const Navbar = () => {
   return (
-    <div>
-        <h1>DevJobs</h1>
-        <div>
-            <i>Icon</i>
-            <i>Icon</i>
-            <i>Icon</i>
+    <div style={{backgroundImage:`url(${bgPatternHeader})`}} className='flex justify-between text-center  px-36 py-10 bg-no-repeat bg-cover rounded-bl-full ' >
+        <img src={logo} alt="" />
+        <div className='hidden  text-2xl text-white md:flex'>
+            <BsFillSunFill className='mx-4'/>
+            <BsToggleOff className='mx-4'/>
+            <BsFillMoonFill className='mx-4'/>
         </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      lightBlue:'#5964E0',
+      white:'#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
